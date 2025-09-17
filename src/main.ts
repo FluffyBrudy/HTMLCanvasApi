2ffd5ac92cc8a7222fa19fbe3ddc140875e70ef7
rudy <fluffyrudy@proton.me>
2025-09-17 21:51:45 +0545

    feat: add trailing effect

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,55 @@
-import { CircularPushedParticle } from "./particle";
+import { CircularPushedParticle, Particle } from "./particle";
 import "./style.css";
+import { randint } from "./utils";
 
 const canvas = document.querySelector("canvas")!;
 const ctx = canvas.getContext("2d")!;
 const mouse = { x: -1, y: -1 };
+const particleArray = new Set<Particle>();
 
 setAttrs(canvas);
 window.onresize = () => setAttrs(canvas);
 canvas.addEventListener("click", (e) => {
+  setLocalMousePos(e);
+  new CircularPushedParticle(mouse.x, mouse.y, 5, { x: 5, y: 5 }, 1);
+  for (let i = 0; i < 10; i++) {
+    particleArray.add(
+      new Particle({
+        x: mouse.x,
+        y: mouse.y,
+      })
+    );
+  }
+});
+canvas.addEventListener("mousemove", (e) => {
+  setLocalMousePos(e);
+  new CircularPushedParticle(
+    mouse.x,
+    mouse.y,
+    randint(0, 5),
+    { x: 5, y: 5 },
+    1
+  );
+
+  for (let i = 0; i < 50; i++) {
+    particleArray.add(
+      new Particle({
+        x: mouse.x,
+        y: mouse.y,
+      })
+    );
+  }
+});
+
+function setAttrs(canvas: HTMLCanvasElement) {
+  const minDimension = Math.min(window.innerWidth, window.innerHeight);
+  canvas.width = minDimension;
+  canvas.height = minDimension;
+  ctx.fillStyle = "#000000";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function setLocalMousePos(e: MouseEvent | PointerEvent) {
   const { left, top } = canvas.getBoundingClientRect();
   const localX = e.clientX - left;
   const localY = e.clientY - top;
@@ -20,31 +62,36 @@ canvas.addEventListener("click", (e) => {
     return;
   mouse.x = localX;
   mouse.y = localY;
-  new CircularPushedParticle(mouse.x, mouse.y, 5, { x: 5, y: 5 }, 1);
-});
-
-function main() {
-  animate();
-}
-
-main();
-
-function setAttrs(canvas: HTMLCanvasElement) {
-  const minDimension = Math.min(window.innerWidth, window.innerHeight);
-  canvas.width = minDimension;
-  canvas.height = minDimension;
-  ctx.fillStyle = "#000000";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function animate() {
-  ctx.fillStyle = "#000000";
-  if (Math.random() < 0.5) {
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }
+  Particle.hue = (Particle.hue + 2) % 360;
+  console.log(Particle.hue);
+  ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   CircularPushedParticle.getParticles().forEach((particle) => {
     particle.update(canvas.width, canvas.height);
     particle.draw(ctx);
   });
+  handleParticles();
   requestAnimationFrame(animate);
 }
+
+function init() {
+  for (let i = 0; i < 1; i++) {
+    particleArray.add(new Particle({ x: mouse.x, y: mouse.y }));
+  }
+}
+
+function handleParticles() {
+  for (let particle of particleArray) {
+    particle.update();
+    particle.draw(ctx);
+    if (particle.isOutOfBound(canvas.width, canvas.height)) {
+      particleArray.delete(particle);
+    } else if (particle.getSize() <= 0) [particleArray.delete(particle)];
+  }
+}
+
+init();
+animate();
--- src/particle.ts
+++ src/particle.ts
@@ -1,44 +1,50 @@
-import { generateRandomColor, randint } from "./utils";
+import { randint } from "./utils";
+
+interface ParticleOptions {
+  x: number;
+  y: number;
+  size?: number;
+  speed?: { x: number; y: number };
+  ignoreOutOfBound?: boolean;
+}
 
 export class Particle {
+  public static hue = 0;
   private x: number;
   private y: number;
-  private initPos: { x: number; y: number };
   private size: number;
   private speed: { x: number; y: number };
-  private color: string;
-  private strokeColor: string;
-  private strokeWidth: number;
+  private ignoreOutOfBound: boolean;
 
-  constructor(
-    x: number,
-    y: number,
-    size?: number,
-    speed?: { x: number; y: number },
-    color?: string,
-    strokeColor?: string,
-    strokeWidth?: number
-  ) {
-    const angle = Math.random() * 2 * Math.PI;
+  constructor({
+    x,
+    y,
+    size,
+    speed,
+    ignoreOutOfBound = false,
+  }: ParticleOptions) {
     this.y = y;
     this.x = x;
-    this.initPos = { x, y };
-    this.size = size || randint(5, 10);
+    this.size = size || randint(1, 8);
     this.speed = speed || {
-      x: this.size * Math.cos(angle),
-      y: this.size * Math.sin(angle),
+      x: Math.random() * 3 - 1.5,
+      y: Math.random() * 3 - 1.5,
     };
-    this.color = color || generateRandomColor();
-    this.strokeColor = strokeColor || generateRandomColor();
-    this.strokeWidth = strokeWidth || randint(1, 5);
+    this.ignoreOutOfBound = ignoreOutOfBound;
   }
 
   public update() {
     this.x += this.speed.x;
     this.y += this.speed.y;
+    this.size = Math.max(0, this.size - 0.1);
+  }
+
+  public getSize() {
+    return this.size;
   }
 
   public isOutOfBound(width: number, height: number) {
+    if (this.ignoreOutOfBound) return false;
     return (
       this.x < -this.size ||
       this.x > width + this.size ||
@@ -48,12 +54,9 @@ export class Particle {
   }
 
   public draw(ctx: CanvasRenderingContext2D) {
-    ctx.fillStyle = this.color;
-    ctx.strokeStyle = this.strokeColor;
-    ctx.lineWidth = this.strokeWidth;
+    ctx.fillStyle = `hsl(${Particle.hue},100%,65%)`;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-    ctx.lineTo(this.initPos.x, this.initPos.y);
     ctx.fill();
     ctx.stroke();
     ctx.closePath();
@@ -69,24 +72,23 @@ export class CircularPushedParticle {
     y: number,
     size: number,
     speed: { x: number; y: number },
-    strokeWidth: number,
-    fromCenter?: number,
-    color?: string,
-    strokeColor?: string
+    fromCenter?: number
   ) {
     this.particleGroup = new Set<Particle>();
     const originFrom = fromCenter || size * 2;
-    for (let i = 0; i < 360; i += 10) {
+    for (let i = 0; i < 360; i += 45) {
       const radian = (i * Math.PI) / 180;
-      const particle = new Particle(
-        x + originFrom * Math.cos(radian),
-        y + originFrom * Math.sin(radian),
+
+      const particle = new Particle({
+        x: x + originFrom * Math.cos(radian),
+        y: y + originFrom * Math.sin(radian),
         size,
-        { x: speed.x * Math.cos(radian), y: speed.y * Math.sin(radian) },
-        color || generateRandomColor(),
-        strokeColor || generateRandomColor(),
-        strokeWidth
-      );
+        speed: {
+          x: speed.x * Math.cos(radian),
+          y: speed.y * Math.sin(radian),
+        },
+      });
+
       this.particleGroup.add(particle);
     }
     CircularPushedParticle.particles.push(this);
@@ -105,7 +107,7 @@ export class CircularPushedParticle {
       particle.update();
       if (particle.isOutOfBound(width, height)) {
         this.particleGroup.delete(particle);
-      }
+      } else if (particle.getSize() <= 1) this.particleGroup.delete(particle);
     }
 
     CircularPushedParticle.particles = CircularPushedParticle.particles.filter(
